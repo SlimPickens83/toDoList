5cb4ab1acae188e567927a14c65a2f744fed639e
Mike Radcliffe <mikerad83@gmail.com>
2022-12-20 14:52:22 -0500

    Began work on basic list item logic.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const entry = document.getElementById('entryForm');
 const form = document.createElement('button');
+let inputTitle;
+let inputDesc;
 
+form.setAttribute('id', 'theForm');
+form.addEventListener('click', () => {
+    inputTitle = prompt("Enter a title:");
+    inputDesc = prompt("Enter a description:");
+});
 entry.appendChild('form');
 
 const actionFactory = (title, desc) => {
     const actionItem = () => document.write(`${title}, ${desc}`);
-    return { title, desc };
+    return { title, desc, actionItem };
 };
